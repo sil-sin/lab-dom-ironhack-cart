d8c9f844e340037e637b6407841bb87f76ae501b
Silvi Sinanaj <silviberat@gmail.com>
2021-03-30 21:08:36 +0200

    Finished Iteration 3 , one question.

--- js/index.js
+++ js/index.js
@@ -1,30 +1,49 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
+  console.log("Calculating subtotal, yey!");
 
-  //... your code goes here
+  let quantity = product.querySelector(".quantity input");
+  let price = product.querySelector(".price span");
+  let subtotal = product.querySelector(".subtotal span");
+  subtotal.innerText = Number(quantity.value * price.innerText);
+  // console.log(subtotal.innerText)
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // iteration 1
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
+
   // end of test
 
   // ITERATION 2
   //... your code goes here
+  let bothProducts = document.querySelectorAll(".product");
+  bothProducts.forEach((item) => {
+    updateSubtotal(item);
+  });
 
   // ITERATION 3
-  //... your code goes here
+
+  let totalPrice = document.querySelector("#total-value span ");
+
+  let subtotal = document.querySelectorAll(".subtotal span");
+
+  subtotal.forEach((span, index) => {
+    totalPrice.innerText = Number(span.innerText) + Number(span.innerText);
+    console.log(totalPrice);
+  });
+  // Question > Why my Total doesnt go back to zero?
 }
 
 // ITERATION 4
 
 function removeProduct(event) {
   const target = event.currentTarget;
-  console.log('The target in remove is:', target);
+  console.log("The target in remove is:", target);
   //... your code goes here
 }
 
@@ -34,9 +53,9 @@ function createProduct() {
   //... your code goes here
 }
 
-window.addEventListener('load', () => {
-  const calculatePricesBtn = document.getElementById('calculate');
-  calculatePricesBtn.addEventListener('click', calculateAll);
+window.addEventListener("load", () => {
+  const calculatePricesBtn = document.getElementById("calculate");
+  calculatePricesBtn.addEventListener("click", calculateAll);
 
   //... your code goes here
 });
